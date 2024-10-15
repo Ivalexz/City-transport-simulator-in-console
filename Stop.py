66604c04df5525d7663191608fb15915a92c4bd6
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2024-10-15 10:32:51 +0300

    Доробив файл Stop

--- Stop.py
+++ Stop.py
@@ -1,9 +1,27 @@
 import CategoriesOfTransport
 import Transport
+import random
 
 class Stop(Transport):
     def __init__(self, pathLength, numberOfStops):
         self.pathLength = pathLength
         self.numberOfStops = numberOfStops
+
+    def time(self, speed):
+
+        arrivalTime = (self.pathLength * self.numberOfStops) / speed
+
+        ran = random.randint(0,1)
+        if ran == 0 :
+            arrivalTime += random.randint(0, 5)
+        else:
+            arrivalTime -= random.randint(0,5)
+
+        return arrivalTime
+
+
+
+
+
 
 
