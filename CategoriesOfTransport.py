d6621ed0248efb20e49b96e6e0d2329f03b45865
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2024-10-16 13:19:20 +0300

    Добавив час на зупинки та перевів в хвилини на дорогу.

--- CategoriesOfTransport.py
+++ CategoriesOfTransport.py
@@ -10,13 +10,17 @@ class Bus(Transport):
 
     def time(self):
 
-        self.arrivalTime = (self.pathLength * self.numberOfStops) / self.speed
+        self.arrivalTime = (self.pathLength * self.numberOfStops) / (self.speed * 3.6)
 
         ran = random.randint(0,1)
         if ran == 0 :
             self.arrivalTime += random.randint(0, 5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
         else:
             self.arrivalTime -= random.randint(0,5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
 
     def showInfoBus(self):
         print(self.model, self.speed, self.arrivalTime)
@@ -31,13 +35,17 @@ class Trolleybus(Transport):
 
     def time(self):
 
-        self.arrivalTime = (self.pathLength * self.numberOfStops) / self.speed
+        self.arrivalTime = (self.pathLength * self.numberOfStops) / (self.speed * 3.6)
 
         ran = random.randint(0,1)
         if ran == 0 :
             self.arrivalTime += random.randint(0, 5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
         else:
             self.arrivalTime -= random.randint(0,5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
 
     def showInfoTrolleybus(self):
         print(self.model, self.speed, self.arrivalTime)
@@ -51,22 +59,26 @@ class Tram(Transport):
 
     def time(self):
 
-        self.arrivalTime = (self.pathLength * self.numberOfStops) / self.speed
+        self.arrivalTime = (self.pathLength * self.numberOfStops) / (self.speed * 3.6)
 
         ran = random.randint(0,1)
         if ran == 0 :
             self.arrivalTime += random.randint(0, 5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
         else:
             self.arrivalTime -= random.randint(0,5)
+            self.arrivalTime /= 60
+            self.arrivalTime += (3 * self.numberOfStops)
 
     def showInfoTram(self):
         print(self.model, self.speed, self.arrivalTime)
 
 
 
-obj = Bus('Nissan', 60, 600, 6)
-obj1 = Trolleybus('Ford', 20, 600, 6)
-obj2 = Tram('Tram', 45, 600,6)
+obj = Bus('Nissan', 35, 600, 6)
+obj1 = Trolleybus('Ford', 20, 600, 8)
+obj2 = Tram('Tram', 40, 600,10)
 
 obj.time()
 obj1.time()
@@ -75,5 +87,6 @@ obj2.time()
 obj.showInfoBus()
 obj1.showInfoTrolleybus()
 obj2.showInfoTram()
+
 
 
